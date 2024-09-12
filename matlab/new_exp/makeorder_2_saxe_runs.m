7dfb0b2ff388575cc02e853bf92006e3faed6165
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-09-12 14:30:03 -0700

    Update makeorder_2_saxe_runs.m

--- matlab/new_exp/makeorder_2_saxe_runs.m
+++ matlab/new_exp/makeorder_2_saxe_runs.m
@@ -173,3 +173,83 @@ for r = 1:nruns
     
     fclose(fid); % Close the file after writing
 end
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Write CSV to be used to make par compatible w/ mrVista
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Path to the directory containing video files
+stim_directory = fullfile('/Users', user, 'Desktop', 'bbfloc',  'stimuli', 'saxestim_wfixation');
+for r = 1:nruns
+
+    % First Determine the run number based off the exp version
+    if version == 1
+        run_number = (r == 1) * 1 + (r ~= 1) * 4; 
+    elseif version == 2
+        run_number = (r == 1) * 2 + (r ~= 1) * 5;
+    elseif version == 3
+        run_number = (r == 1) * 3 + (r ~= 1) * 6;
+    end
+    
+    csvfilename = fullfile(participant_folder, strcat('saxe_parfile_for_run', num2str(run_number), '.csv'));
+    fid = fopen(csvfilename, 'w');
+    if fid == -1
+        error('Failed to open file: %s', csvfilename);
+    end
+    fprintf(fid, 'Block #,Onset-time(s),Category,TaskMatch,Video Name,Video Path,Video Dur\n');
+
+    onset_time = 0; 
+    current_block = 1; 
+    
+    % Write initial baseline block
+    write_baseline_block_dynamic(fid, current_block, onset_time, stim_directory);
+    onset_time = onset_time + 4 * 2; % Adjusted for 2 blank trials
+    current_block = current_block + 1;
+    
+    % Loop through trials
+    for i = 1:ntrials 
+        original_category_index = condmat(i, r);
+        mapped_category_index = category_mapping(original_category_index); 
+        vid_name = stimmat{i, r};
+        stim_path = fullfile(stim_dir, vid_name);
+        
+        % Determine if the current trial is the last in the block
+        if mod(i,6) == 0
+            % Reduce the duration of the last trial by 0.0002 seconds
+            fprintf(fid, '%i,%f,%i,%s,%s,%s,%f\n', ...
+                current_block, onset_time, mapped_category_index, cats{original_category_index}, vid_name, stim_path, viddur - 0.0002);
+        else
+            fprintf(fid, '%i,%f,%i,%s,%s,%s,%f\n', ...
+                current_block, onset_time, mapped_category_index, cats{original_category_index}, vid_name, stim_path, viddur);
+        end
+
+        % Update onset time based on trial
+        if mod(i,6) == 0
+            onset_time = onset_time + (viddur - 0.0001);
+        elseif mod(i,3) == 0
+            current_block = current_block + 1;
+            onset_time = onset_time + (viddur - 0.0001);
+        else
+            onset_time = onset_time + viddur;
+        end
+
+        % Insert blank block if condmat changes 
+        if i < ntrials && condmat(i, r) ~= condmat(i + 1, r) 
+            current_block = current_block + 1;
+            write_baseline_block_dynamic(fid, current_block, onset_time, stim_directory);
+            onset_time = onset_time + 4 * 2; % Adjusted for 2 blank trials
+            current_block = current_block + 1;
+        end
+    end
+
+    % Write closing baseline block
+    current_block = current_block + 1;
+    write_baseline_block_dynamic(fid, current_block, onset_time, stim_directory);
+    onset_time = onset_time + 4 * 2; % Adjusted for 2 blank trials
+    current_block = current_block + 1;
+    
+    fclose(fid); % Close the file after writing
+end
+
+
+
